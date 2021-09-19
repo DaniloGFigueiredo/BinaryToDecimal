91faf49d91a46d0a5440d294912b182d45bedb46
DaniloGFigueiredo <danilo.figueiredo@stone.com.br>
2021-09-19 12:05:22 -0300

    Add project files.

---
+++ BinaryToDecimal.Service/BinaryToDecimalService.cs
@@ -0,0 +1,49 @@
+﻿using BinaryToDecimal.Domain;
+using System;
+
+namespace BinaryToDecimal.Service
+{
+    public sealed class BinaryToDecimalService : IBinaryToDecimalService
+    {
+        public string TransformBinaryToDecimal(string binarySequence)
+        {
+            //teste
+            var isValid = ValidateBinarySequence(binarySequence);
+
+            if (isValid)
+            {
+                var decimalValue = new double();
+
+                for (int index = binarySequence.Length - 1; index >= 0; index--)
+                {
+                    var binaryDigit = binarySequence[index].ToString();
+                    var toThePowerOfExpression=  Math.Pow(2, binarySequence.Length - 1 - index);
+                    decimalValue += Convert.ToInt32(binaryDigit) * toThePowerOfExpression; 
+                }
+                return decimalValue.ToString();
+            }
+            else
+            {
+                return "O sistema apenas aceita valores binarios '1' ou '0'";
+            }
+        }
+
+        private static bool ValidateBinarySequence(string binarySequence)
+        {
+            foreach (char input in binarySequence)
+            {
+                if (input == '0' || input == '1')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }               
+            }
+
+            return true;
+        }
+    }
+}
+
---
+++ BinaryToDecimal/Program.cs
@@ -0,0 +1,57 @@
+﻿using BinaryToDecimal.Domain;
+using BinaryToDecimal.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace BinaryToDecimal
+{
+    sealed class Program
+    {
+        private readonly IBinaryToDecimalService _programService;
+        //teste
+        public Program(IBinaryToDecimalService binaryToDecimalService)
+        {
+            _programService = binaryToDecimalService;
+        }
+
+        static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Program>().Run();
+        }
+
+        private void Run()
+        {
+            var stopRunning = false;
+            while (stopRunning == false)
+            {
+                Console.WriteLine("Digite uma sequência de números binários");
+                var binarySequence = Console.ReadLine();
+                var result = _programService.TransformBinaryToDecimal(binarySequence);
+                Console.WriteLine(result);
+                Console.WriteLine("Deseja parar?  S/N");
+                var response = Console.ReadLine().ToUpper();
+                if (response.Equals("S"))
+                {
+                    stopRunning = true;
+                    Environment.Exit(0);
+                }
+                else if (response.Equals("N"))
+                {
+                    continue;
+                }                    
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<Program>();
+                    services.AddTransient<IBinaryToDecimalService, BinaryToDecimalService>();
+                });
+        }
+    }
+}
